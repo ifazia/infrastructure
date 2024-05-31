9445497ee1d98d095677012bfa7b163e19f6a20d
ifazia <iguetf@gmail.com>
2024-05-31 22:53:32 +0200

    deplacement infra et deploys

--- .github/workflows/Terraform-eks-cluster-main/terraform-projet-petclinic/deploy.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-terraform apply -auto-approve
-export cluster_name=petclinic-eks-cluster
-oidc_id=$(aws eks describe-cluster --name $cluster_name --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
-aws iam list-open-id-connect-providers | grep $oidc_id | cut -d "/" -f4
-eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve
-aws iam list-open-id-connect-providers | grep $oidc_id | cut -d "/" -f4
