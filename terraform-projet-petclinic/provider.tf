5e369bdb906b63527b37cdb50d5452cf38b0ff4a
ifazia <iguetf@gmail.com>
2024-06-21 16:16:07 +0200

    modif infra

--- terraform-projet-petclinic/provider.tf
+++ terraform-projet-petclinic/provider.tf
@@ -1,10 +1,34 @@
 terraform {
   required_providers {
+   terraform {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.16.2"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.13.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
+}
+}
 }
 
 provider "aws" {
